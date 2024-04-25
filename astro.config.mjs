368699de04c6691d4e39b1605a1444b1d623eaa2
ERHDDEV <erhgdev@gmail.com>
2024-04-25 15:11:32 -0600

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind()],
   site: 'https://erhg.dev/'
 })
